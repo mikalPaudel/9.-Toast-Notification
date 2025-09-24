8b966c9d59ca0d796b9c0cbf556076f4f3bc7ea7
Mikal Paudel <mikalpaudel58@gmail.com>
2025-09-24 21:44:25 +0545

    Enhance toast notifications with icons and improved styles; update HTML and CSS for better visual feedback

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-let success = 'Successfully done';
-let error = 'Error occurred';
-let invalid = 'Invalid input';
+let success = '<i class="fa-solid fa-circle-check"></i>Successfully done';
+let error = '<i class="fa-solid fa-circle-xmark"></i>Error occurred';
+let invalid = '<i class="fa-solid fa-triangle-exclamation"></i>Invalid input';
 function showToast(msg) {
     const toasts = document.querySelector('.toasts');
     const toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerText = msg;
+    toast.innerHTML = msg;
     toasts.appendChild(toast);
+
+    if (msg.includes('Successfully done')) {
+        toast.classList.add('success');
+    } else if (msg.includes('Error occurred')) {
+        toast.classList.add('error');
+    } else if (msg.includes('Invalid input')) {
+        toast.classList.add('invalid');
+    }
 
     setTimeout(() => {
         toast.remove();
-    }, 3000);
+    }, 5000);
 }
